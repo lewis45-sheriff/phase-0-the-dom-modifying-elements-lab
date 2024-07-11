de6bac6091ca2b1a8964091713eaa9513365be44
lewis <lewiskipkemoi53@gmail.com>
2024-07-11 12:04:34 +0300

    all

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+    main.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Lewis Kipkemoi Cheruiyot is the champion';
+newHeader.style.color='blue'
+
+
+
+document.body.appendChild(newHeader);
